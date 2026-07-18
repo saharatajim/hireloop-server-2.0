dda6dbed5c54d8cbf3d259a5b03f597f9e2b09f5
saharatajim <sahara.tajim@gmail.com>
2026-07-18 16:25:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors=require('cors')
+const app = express()
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI
+const port = 5000
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+         const db = client.db("hireloopdb");
+         const jobsCollection=db.collection("jobs")
+
+       app.get('/api/jobs', async (req, res) => {
+  const query = {};
+
+  if (req.query.companyId) {
+    query.companyId = req.query.companyId;
+  }
+  if (req.query.status) {
+    query.status = req.query.status;
+  }
+
+  const cursor = jobsCollection.find(query);
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+         app.post('/api/jobs',async(req,res)=>{
+
+            const job=req.body
+            const result=await jobsCollection.insertOne(job)
+             
+         })
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
